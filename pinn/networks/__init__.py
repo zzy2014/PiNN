038bd1fa8f42d05d95595188677df7acd23fd067
Roy Kid <lijichen365@gmail.com>
2024-05-23 12:29:36 +0200

    add p5 network: p5 dot represents p5 as 3x3 mat, p5 irrep as 5 elems, p5 irrep channel is only use 4 channel for p5

--- pinn/networks/__init__.py
+++ pinn/networks/__init__.py
@@ -11,11 +11,17 @@ def get(network_spec):
     from pinn.networks.bpnn import BPNN
     from pinn.networks.lj import LJ
     from pinn.networks.pinet2 import PiNet2
+    from pinn.networks.pinet2_p5_dot import PiNet2P5Dot
+    from pinn.networks.pinet2_p5_irrep_dot import PiNet2P5IrrepDot
+    from pinn.networks.pinet2_p5_irrep_dot_channel import PiNet2P5IrrepDotChannel
     implemented_networks = {
         'PiNet': PiNet,
         'BPNN': BPNN,
         'LJ': LJ,
-        'PiNet2': PiNet2
+        'PiNet2': PiNet2,
+        'PiNet2P5Dot': PiNet2P5Dot,
+        'PiNet2P5IrrepDot': PiNet2P5IrrepDot,
+        'PiNet2P5IrrepDotChannel': PiNet2P5IrrepDotChannel
     }
     if isinstance(network_spec, tf.keras.Model):
         return network_spec
